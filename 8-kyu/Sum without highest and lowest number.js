0e9c661422e0446c8d9adc924765db8ab49a5219
Shamil Omarov <s_omarov05@mail.ru>
2022-07-17 20:54:54 +0300

    Sum without highest and lowest number #2

--- 8-kyu/Sum without highest and lowest number.js
+++ 8-kyu/Sum without highest and lowest number.js
@@ -30,3 +30,20 @@ function sumArray(array) {
 }
 console.log(sumArray([6, 2, 1, 8, 10]));
 console.log(sumArray([1, 1, 11, 2, 3]));
+
+
+
+// PART #2
+function sumArray(array) {
+  if(!array || array.length < 2) return 0;
+  let min = array[0];
+  let max = array[0];
+  let sum = array[0];
+  
+  for (let i = 1; i < array.length; i += 1) {
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+    sum += array[i];
+  }
+  return sum - min - max;
+}
