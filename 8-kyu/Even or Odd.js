427bdb20c893cf17d56a03e002dd47eeb1642521
Shamil Omarov <s_omarov05@mail.ru>
2022-07-18 19:56:08 +0300

    even_or_odd

---
+++ 8-kyu/Even or Odd.js
@@ -0,0 +1,15 @@
+/*
+DESCRIPTION:
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+#MATHEMATICS #FUNDAMENTALS
+*/
+
+function even_or_odd(number) {
+  if(number % 2 === 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
+  }
+}
