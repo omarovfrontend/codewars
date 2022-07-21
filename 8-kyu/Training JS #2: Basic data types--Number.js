2d297f9922b42050ce0d043ac0a0c1832d8bde20
Shamil Omarov <s_omarov05@mail.ru>
2022-07-21 18:04:30 +0300

    Training JS #2: Basic data types--Number #2

--- 8-kyu/Training JS #2: Basic data types--Number.js
+++ 8-kyu/Training JS #2: Basic data types--Number.js
@@ -41,3 +41,17 @@ function equal5(){
   let b = v3;
   return a % b;
 }
+
+// Part #2
+let v1 = 50, 
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+const equal1 = () => v1 + v1;
+const equal2 = () => v4 - v2;
+const equal3 = () => v5 * v1;
+const equal4 = () => v4 / v5;
+const equal5 = () => v6 % v3;
