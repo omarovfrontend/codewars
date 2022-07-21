46d2f33ed2db514a8148b12e33e90193ca7c50c2
Shamil Omarov <s_omarov05@mail.ru>
2022-07-21 19:08:26 +0300

    Training JS #4: Basic data types--Array #2

--- 8-kyu/Training JS #4: Basic data types--Array.js
+++ 8-kyu/Training JS #4: Basic data types--Array.js
@@ -51,3 +51,16 @@ function popElement(arr){
   arr.pop();                   // метод pop - удаляет последний элемент из массива и возвр-ет новую длину []
   return arr;
 }
+
+// Part #2
+const getLength = (arr) => arr.length;
+const getFirst = (arr) => arr[0];
+const getLast = (arr) => arr[arr.length - 1];
+const pushElement = (arr) => {
+  arr.push(1);
+  return arr;
+}
+const popElement = (arr) => {
+  arr.pop();
+  return arr;
+}
