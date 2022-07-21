67b9aaccd600b41435693309b25a122f347bb729
Shamil Omarov <s_omarov05@mail.ru>
2022-07-21 18:30:02 +0300

    Training JS #3: Basic data types--String

---
+++ 8-kyu/Training JS #3: Basic data types--String.js
@@ -0,0 +1,37 @@
+/*
+DESCRIPTION:
+In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+
+String can use operators +, Connect two or more strings together.
+
+Task
+misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
+
+If everything is right, click "Submit" again to submit your code pass this kata.
+
+#FUNDAMENTALS #TUTORIALS
+*/
+
+let a1 = "A",
+    a2 = "a",
+    b1 = "B",
+    b2 = "b",
+    c1 = "C",
+    c2 = "c",
+    d1 = "D",
+    d2 = "d",
+    e1 = "E",
+    e2 = "e",
+    n1 = "N",
+    n2 = "n";
+
+const Dad = () => d1 + a2 + d2;
+const Bee = () => b1 + e2 + e2;
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2;
+const answer1 = () => "no";
+const answer2 = () => "no";
+const answer3 = () =>  "yes";
