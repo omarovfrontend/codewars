4b0679d56497309974bf0d32defdfe835faadc86
Shamil Omarov <s_omarov05@mail.ru>
2022-07-21 20:49:49 +0300

    Beginner - Lost Without a Map

---
+++ 8-kyu/Beginner - Lost Without a Map.js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+#FUNDAMENTALS #ARRAYS
+*/
+
+function maps(x) {
+  return x.map(el => el * 2);
+}
