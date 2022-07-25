a5cb7a2c969f8305fef89204a8e0d08b1075a2b9
Shamil Omarov <s_omarov05@mail.ru>
2022-07-26 02:29:52 +0300

    Training JS #13: Number object and its properties #2

--- 8-kyu/Training JS #13: Number object and its properties.js
+++ 8-kyu/Training JS #13: Number object and its properties.js
@@ -57,3 +57,21 @@ function whatNumberIsIt(n) {
     return `Input number is ${n}`;
   }
 }
+
+// Part #2
+function whatNumberIsIt(n) {
+  switch(n) {
+    case Infinity:
+      return 'Input number is Number.POSITIVE_INFINITY';
+    case -Infinity:
+      return 'Input number is Number.NEGATIVE_INFINITY';
+    case 1.7976931348623157e+308:
+      return 'Input number is Number.MAX_VALUE';
+    case 5e-324:
+      return 'Input number is Number.MIN_VALUE';
+    case n:
+      return `Input number is ${n}`;
+    default:
+      return 'Input number is Number.NaN';
+  }
+}
