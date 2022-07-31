e9ee35353b57e04f2cae94b224c071c75df03e72
Shamil Omarov <s_omarov05@mail.ru>
2022-08-01 01:09:59 +0300

    Fake Binary

---
+++ 8-kyu/Fake Binary.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION:
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+#FUNDAMENTALS #STRINGS #ARRAYS
+*/
+
+function fakeBin(x) {
+  let res = '';
+  
+  for (let i = 0; i < x.length; i += 1) {
+    (x[i] < 5) ? res += 0 : res += 1;
+  }
+  return res;
+}
+
+// Part #2
+function fakeBin(x) {
+  let res = '';
+  
+  for (let i = 0; i < x.length; i += 1) {
+    if (x[i] < 5) {
+      res += 0;
+    } else {
+      res += 1;
+    } 
+  }
+  return res;
+}
