46072c0314682057d5195dbec6bd83fc725d4968
Shamil Omarov <s_omarov05@mail.ru>
2022-08-02 23:39:02 +0300

    Calculate average #2

--- 8-kyu/Calculate average.js
+++ 8-kyu/Calculate average.js
@@ -11,3 +11,14 @@ function find_average(arr) {
   if (arr.length === 0) return 0;
   return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
+
+// Part #2
+function find_average(arr) {
+  if (arr.length === 0) return 0;
+  
+  let res = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    res += arr[i];
+  }
+  return res / arr.length;
+}
