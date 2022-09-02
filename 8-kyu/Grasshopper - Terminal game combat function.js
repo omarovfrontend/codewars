52ccae30c1acda20d5cec0b5641fb8adc98c2549
Shamil Omarov <s_omarov05@mail.ru>
2022-09-02 05:06:07 +0300

    Grasshopper - Terminal game combat function

---
+++ 8-kyu/Grasshopper - Terminal game combat function.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+#FUNDAMENTALS
+*/
+
+function combat(health, damage) {
+  return health > damage ? health - damage : 0;
+}
