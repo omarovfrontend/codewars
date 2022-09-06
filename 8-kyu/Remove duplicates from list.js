1e5805903ade198d3ca3e38590341186eca57247
Shamil Omarov <s_omarov05@mail.ru>
2022-09-06 20:19:27 +0300

    Remove duplicates from list

---
+++ 8-kyu/Remove duplicates from list.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+#FUNDAMENTALS
+*/
+
+function distinct(a) {
+  return a.filter((el, i) => a.indexOf(el) === i);
+}
