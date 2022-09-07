aff2a0d3248032d4b5501a09504a7139a09d665d
Shamil Omarov <s_omarov05@mail.ru>
2022-09-07 19:41:14 +0300

    Hex to Decimal

---
+++ 8-kyu/Hex to Decimal.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+#FUNDAMENTALS
+*/
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
