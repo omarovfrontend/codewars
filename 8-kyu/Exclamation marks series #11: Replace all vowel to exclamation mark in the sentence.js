ef5a8f697d7adf121a1c9c3ed35e559113286136
Shamil Omarov <s_omarov05@mail.ru>
2022-09-07 20:39:05 +0300

    Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence #2

--- 8-kyu/Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.js
+++ 8-kyu/Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.js
@@ -15,3 +15,24 @@ replace("ABCDE") === "!BCD!"
 function replace(s) {
   return s.replace(/[aeiouAEIOU]/g, '!');
 }
+
+// Part #2
+function replace(s) {
+  const sArr = s.split('');
+  
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === 'a' ||
+        s[i] === 'e' ||
+        s[i] === 'i' ||
+        s[i] === 'o' ||
+        s[i] === 'u' ||
+        s[i] === 'A' ||
+        s[i] === 'E' ||
+        s[i] === 'I' ||
+        s[i] === 'O' ||
+        s[i] === 'U') {
+      sArr[i] = '!';
+    }
+  }
+  return sArr.join('');
+}
