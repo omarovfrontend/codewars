d6129557d27127120a8eb0c3211d866433402e54
Shamil Omarov <s_omarov05@mail.ru>
2022-09-08 20:24:04 +0300

    Bin to Decimal

---
+++ 8-kyu/Bin to Decimal.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+#BINARY #FUNDAMENTALS
+*/
+
+function binToDec(bin){
+  return parseInt(bin, 2);
+}
