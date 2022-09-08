71612a6a7c0b053b1f7e18466f0ed2a2e2e6e3c7
Shamil Omarov <s_omarov05@mail.ru>
2022-09-08 21:05:52 +0300

    Hello, Name or World!

---
+++ 8-kyu/Hello, Name or World!.js
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"                  => return "Hello, John!"
+* With `name` = "aliCE"                 => return "Hello, Alice!"
+* With `name` not given or `name` = ""  => return "Hello, World!"
+
+#FUNDAMENTALS
+*/
+
+function hello(name) {
+  return (name === '' || name === undefined) ? `Hello, World!` :
+  `Hello, ${name.slice(0, 1).toUpperCase()}${name.slice(1).toLowerCase()}!`;
+}
