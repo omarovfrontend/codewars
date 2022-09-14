eee66c17842e0d6d8035d4b2c03f7420476e89b5
Shamil Omarov <s_omarov05@mail.ru>
2022-09-15 01:06:46 +0300

    The (un)defined truth

---
+++ 6-kyu/The (un)defined truth.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+You'll have to make a function output return the word defined but without using:
+
+void 0, undefined, var, let, const, regex and eval
+String, Boolean, Number, Array, Object, Function
+Literals for strings (single, double and backticks), objects, arrays, numbers and operators +, -, /, * (means no JsF*ck you)
+((un)?defined) anywhere
+
+#PUZZLES #GAMES
+*/
+
+function output() {
+  return (typeof $).slice(Math.E);
+}
+
