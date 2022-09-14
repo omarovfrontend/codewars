da986df7f060136d7fca63fd219bf0289dabec61
Shamil Omarov <s_omarov05@mail.ru>
2022-09-14 17:45:02 +0300

    Not so black box

--- 8-kyu/Not so black box.js
+++ 8-kyu/Not so black box.js
@@ -1,7 +1,6 @@
 /*
 DESCRIPTION:
-The kata has been preloaded with a function called blackBox. 
-Figure out what the function does.
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
 
 You may need to think laterally!
 
