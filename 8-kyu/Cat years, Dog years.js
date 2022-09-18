db7d70e32d6613cb4af4f6a7e007480339290c7e
Shamil Omarov <s_omarov05@mail.ru>
2022-09-18 18:52:43 +0300

    Cat years, Dog years #2

--- 8-kyu/Cat years, Dog years.js
+++ 8-kyu/Cat years, Dog years.js
@@ -51,3 +51,23 @@ const humanYearsCatYearsDogYears = function(humanYears) {
   }
   return [humanYears, catYears, dogYears];
 }
+
+// Part #2
+const humanYearsCatYearsDogYears = humanYears => {
+  let catYears = 0;
+  let dogYears = 0;
+  
+  for (let i = 1; i <= humanYears; i += 1) {
+    if (i === 1) {
+      catYears += 15;
+      dogYears += 15;
+    } else if (i === 2) {
+      catYears += 9;
+      dogYears += 9;
+    } else {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+  return [humanYears, catYears, dogYears];
+}
