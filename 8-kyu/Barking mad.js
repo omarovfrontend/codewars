9a47d969ca3b43e364f095dbfc0b8a246ff5dc5c
Shamil Omarov <s_omarov05@mail.ru>
2022-09-20 18:08:26 +0300

    Barking mad #2

--- 8-kyu/Barking mad.js
+++ 8-kyu/Barking mad.js
@@ -25,3 +25,16 @@ scoobydoo.bark = function() {
   return "Woof";
 };
 
+// Part #2
+class Dog {
+  constructor(breed) {
+    this.breed = breed;
+  }
+  
+  bark() {
+    return 'Woof';
+  }
+}
+
+const snoopy = new Dog("Beagle");
+const scoobydoo = new Dog("Great Dane");
