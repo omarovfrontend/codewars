86f04d716b0f403df253159088bec13ebddb0621
Shamil Omarov <s_omarov05@mail.ru>
2022-09-20 18:39:51 +0300

    Stringy Strings

---
+++ 8-kyu/Stringy Strings.js
@@ -0,0 +1,25 @@
+/*
+DESCRIPTION:
+write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+the string should start with a 1.
+
+a string with size 6 should return :'101010'.
+
+with size 4 should return : '1010'.
+
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.
+
+#STRINGS #BINARY #ALGORITHMS
+*/
+
+function stringy(size) {
+  let str = '';
+  
+  for (let i = 1; i <= size; i += 1) {
+    str += i % 2;
+  }
+  return str;
+}
