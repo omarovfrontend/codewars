b004efaa8e7f907284442422aa00b7e117e154af
Shamil Omarov <s_omarov05@mail.ru>
2022-09-22 18:51:39 +0300

    Enumerable Magic - Does My List Include This?

---
+++ 8-kyu/Enumerable Magic - Does My List Include This?.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+#FUNDAMENTALS
+*/
+
+function include(arr, item) {
+  return arr.includes(item);
+}
