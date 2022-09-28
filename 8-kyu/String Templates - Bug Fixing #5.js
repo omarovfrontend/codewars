ab27438ca986ef4ad09e9b633cca42e0dcd6b868
Shamil Omarov <s_omarov05@mail.ru>
2022-09-28 16:09:29 +0300

    String Templates - Bug Fixing #5

---
+++ 8-kyu/String Templates - Bug Fixing #5.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+#STRINGS #DEBUGGING
+*/
+
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
