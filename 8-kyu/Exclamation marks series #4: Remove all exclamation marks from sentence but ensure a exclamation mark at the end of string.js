04831ac1c4f13ab3de0b111af298c3d36811e2e3
Shamil Omarov <s_omarov05@mail.ru>
2022-10-03 20:12:51 +0300

    Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string

---
+++ 8-kyu/Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+Description:
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+Examples
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"
+
+#FUNDAMENTALS
+*/
+
+function remove(str) {
+  return str.replace(/!/g, '') + '!';
+}
