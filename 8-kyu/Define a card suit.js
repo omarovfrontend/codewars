3caaea26f7f9d2f3aadf4171f9396e049dcb9484
Shamil Omarov <s_omarov05@mail.ru>
2022-10-03 20:07:25 +0300

    Define a card suit #2

--- 8-kyu/Define a card suit.js
+++ 8-kyu/Define a card suit.js
@@ -20,3 +20,11 @@ function defineSuit(card) {
   else if (strArr[1] === '♦' || strArr[2] === '♦') return 'diamonds';
   else return 'hearts';
 }
+
+// Part #2
+function defineSuit(card) {
+  if (card.includes('♣')) return 'clubs';
+  else if (card.includes('♦')) return 'diamonds';
+  else if (card.includes('♥')) return 'hearts';
+  else return 'spades';
+}
