3e27b93c259c903f66b59096a305fb94bbac2b46
Shamil Omarov <s_omarov05@mail.ru>
2022-10-04 21:45:23 +0300

    Grasshopper - Variable Assignment Debug

---
+++ 8-kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+#FUNDAMENTALS
+*/
+
+const a = "dev";
+const b = "Lab";
+
+const name = a + b;
