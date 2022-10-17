f3b5687ebec0767ce5af94a35377f0d4fe3c789f
Shamil Omarov <s_omarov05@mail.ru>
2022-10-17 23:02:00 +0300

    Hello Happy Codevarrior!

---
+++ 8-kyu/Hello Happy Codevarrior!.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+
+#DEBUGGING
+*/
+
+function Warrior(n) {
+  let name = n;
+  this.name = function(n) {
+    if(n) {
+      return name = n;
+    } 
+    return name;
+  }
+}
+  
+Warrior.prototype.toString = function() {
+  return "Hi! my name's "+ this.name();
+}
