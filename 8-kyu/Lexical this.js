6e4c7a67150eb0de21c1ff54204934c20ba42a90
Shamil Omarov <s_omarov05@mail.ru>
2022-10-21 00:00:02 +0300

    Lexical this

---
+++ 8-kyu/Lexical this.js
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+Lexical this
+Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+Output
+Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+#FUNDAMENTALS
+*/
+
+const Person = function(){
+  const person = {
+    _name: "Leroy",
+    _friends: [],
+    fillFriends(f) {
+      f.forEach(el => this._friends.push(el));
+    }
+  }
+  return person;
+}
